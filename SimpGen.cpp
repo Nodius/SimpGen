aea365d9c1f4298039c24234f052ef3530406924
Nodius <staspalchak@gmail.com>
2016-08-26 14:26:00 +0300

    Add files via upload

---
+++ SimpGen.cpp
@@ -0,0 +1,181 @@
+//SimpGen - Generate season and episode The Simpsons
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	cout << "SimpGen - Season and Episode The Simpsons" << endl;
+	srand(time(0));
+	int season = 1 + rand() % 27;
+	switch (season)
+	{
+	case 1:
+	{
+		int ep = 1 + rand() % 13;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 2:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 3:
+	{
+		int ep = 1 + rand() % 24;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 4:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 5:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 6:
+	{
+		int ep = 1 + rand() % 25;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 7:
+	{
+		int ep = 1 + rand() % 25;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 8:
+	{
+		int ep = 1 + rand() % 25;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 9:
+	{
+		int ep = 1 + rand() % 25;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 10:
+	{
+		int ep = 1 + rand() % 23;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 11:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 12:
+	{
+		int ep = 1 + rand() % 21;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 13:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 14:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 15:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 16:
+	{
+		int ep = 1 + rand() % 21;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 17:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 18:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 19:
+	{
+		int ep = 1 + rand() % 20;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 20:
+	{
+		int ep = 1 + rand() % 21;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 21:
+	{
+		int ep = 1 + rand() % 23;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 22:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 23:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 24:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 25:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 26:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	case 27:
+	{
+		int ep = 1 + rand() % 22;
+		cout << "Season: " << season << endl;
+		cout << "Episode: " << ep << endl;
+		break;}
+	default:
+		cout << "Wrong number!" << endl;
+		break;
+		return 0;
+	}
+}
